250a5acfed0004f919b163cfdd6aca417eecec2c
SAYAN MAITY <sayanmaity007@gmail.com>
2020-04-23 14:57:39 +0530

    shifting done #leetcode

---
+++ leetCode30/stringShift/Main.java
@@ -0,0 +1,19 @@
+
+class Main {
+  static public String stringShift(String s, int[][] shift) {
+    int finalShifting = 0;
+    for(int[] el : shift)
+    {
+      if(el[0]==0)
+      finalShifting -= el[1];
+      else
+      finalShifting += el[1];
+    }
+    return(s.substring(s.length()-finalShifting, s.length())+s.substring(0, 
+        s.length() - finalShifting));
+  }
+  public static void main(String[] args) {
+    int[][] shift = new int[][] {{0,1},{1,2}};
+    System.out.println(stringShift("abc", shift));
+  }
+}
