afa9cf739044b0f84f1a00ac0bd493139ae0b1f6
SAYAN MAITY <sayanmaity007@gmail.com>
2020-04-23 14:45:50 +0530

    bitAnd leetcode

---
+++ leetCode30/bitwiseAND/Main.java
@@ -0,0 +1,11 @@
+
+class Main {
+  static public int rangeBitwiseAnd(int m, int n) {
+    while (m < n)
+      n -= (n & -n);
+    return n;
+  }
+  public static void main(String[] args) {
+    System.out.println(rangeBitwiseAnd(7,4));
+  }
+}
