318141606d6c8a71025d6467410d09078cd67f5a
SAYAN MAITY <sayanmaity007@gmail.com>
2020-05-05 18:55:29 +0530

    #4

---
+++ LeetCode #mayChallenge/firstUnique/Main.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+class Main{
+  public static int firstUnique(String s){
+    HashMap<Character,Integer> map = new HashMap<>();
+    for(char c : s.toCharArray()){
+      if(!map.containsKey(c)){
+        map.put(c, 1);
+      }else{
+        map.put(c, map.get(c)+1);
+      }
+    }
+    for(int i=0;i<s.length();i++){
+      if(map.get(s.charAt(i)) == 1){
+        return i;
+      }
+    }
+    return -1;
+  }
+  public static void main(String[] args) {
+    String S = "leetcode";
+  }
+}
